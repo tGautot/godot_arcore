702b68c0ad37d37a479621d24074302b00a9b586
tgautot <tgautot.off@gmail.com>
2025-02-20 13:38:20 +0100

    Utility component for tracking node to images

--- plugin/export_scripts_template/TrackedToImageComponent.gd
+++ plugin/export_scripts_template/TrackedToImageComponent.gd
@@ -1,13 +1,22 @@
 extends Node
+class_name ImageTrackedComponent
+
+signal onActivated
+signal onDeactivated
 
 var target: Node3D
 
 @export var trackedImage: Image
+@export var trackedTexture: Texture2D
 
 @export_group("Transform")
 @export var position_offset: Vector3 = Vector3(0,0,0)
 
-@export var disabled: bool = false
+@export var maxDistanceRender: float = 2
+
+@export var timeToDeactivate: float = 5
+
+var ttl_activation: float = -1
 
 enum ROTATION_MIX {ABSOLUTE, RELATIVE}
 
@@ -22,11 +31,48 @@ enum ROTATION_MIX {ABSOLUTE, RELATIVE}
 
 func _ready():
 	target = get_parent()
-	ARCoreInterfaceInstance.image_tracker_database_add_image(trackedImage, trackedImage.resource_name)
+	var image_name: String = ""
+	if trackedImage == null:
+		if trackedTexture == null:
+			push_error("ImageTrackedComponent needs either an image or a texture")
+		trackedImage = trackedTexture.get_image()
+	
 
-func _process(_delta: float):
+	ARCoreInterfaceInstance.image_tracker_database_add_image(trackedImage, image_name)
+
+func is_active():
+	return ttl_activation > 0
+
+func _process(delta: float):
+	
+	var validTracking = ARCoreInterfaceInstance.image_tracker_get_tracking_status(trackedImage.resource_name)
+	if validTracking:
+		if ttl_activation < 0:
+			onActivated.emit()
+		ttl_activation = timeToDeactivate
+		target.visible = true
+	else:
+		if ttl_activation > 0:
+			ttl_activation -= delta
+			if ttl_activation < 0:
+				onDeactivated.emit()
+				target.visible = false
+		return
+
+
 	var trackedTrsfrm: Transform3D = ARCoreInterfaceInstance.image_tracker_get_tracked_transform(trackedImage.resource_name)
 	target.position = trackedTrsfrm.origin + position_offset
+	
+	
+	var cam = get_viewport().get_camera_3d()
+	var distToCam = target.global_position.distance_to(cam.global_position)
+	
+	if distToCam > maxDistanceRender:
+		target.visible = false
+		return
+	else:
+		target.visible = true
+	
 	
 	var imgRot = trackedTrsfrm.basis.get_euler()
 	
